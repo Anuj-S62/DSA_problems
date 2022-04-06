4717ff4c20d43b2e053c232d5881df3f5f2c11b3
Anuj Solanki <anuj603362@gmail.com>
2022-04-06 20:28:20 +0530

    added

---
+++ seraching_in_BST.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+    int val;
+    struct node* left;
+    struct node* right;
+}node;
+
+
+node* create(int val){
+   node * root = new node;
+   root->val = val;
+   root->left = NULL;
+   root->right = NULL;
+   return  root;
+}
+
+node* rec_search(node* n,int val){
+    if(n==nullptr){
+        cout<<"Not found"<<endl;
+        return n;
+    }
+    if(n->val == val){
+        cout<<"found"<<endl;
+        return n;
+    }
+    else if(n->val > val){
+        return rec_search(n->left,val);
+    }
+    return rec_search(n->right,val);
+}
+
+node* loop_serach(node* n,int val){
+    while(n){
+        if(n->val == val) return n;
+        else if(n->val > val) n = n->left;
+        else if(n->val<val)n = n->right;
+    }
+    return NULL;
+}
+
+int main(){
+    node* root = create(50);
+    node* n1 = create(30);
+    node* n2 = create(80);
+
+    root->left = n1;
+    root->right = n2;
+
+    node* n3 = create(20);
+    node* n4 = create(40);
+
+    n1->left = n3;
+    n1->right = n4;
+
+    node* n5 = create(60);
+    node* n6 = create(90);
+
+    n2->left = n5;
+    n2->right = n6;
+    node* ptr = root;
+    ptr = loop_serach(ptr,908);
+    if(ptr){
+        cout<<"found"<<endl;
+        cout<<ptr->val<<endl;
+    }
+    else cout<<"NULL"<<endl;
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,71 +1,49 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// void powerset(string a,int i=0,string res=""){
-//     if(i==a.length()){
-//         cout<<res<<endl;
-//         return ;
-//     }
-//     powerset(a,i+1,res+a[i]);
-//     powerset(a,i+1,res);
-// }
-// string swap(string s,int l,int i){
-//     char temp=s[l];
-//     s[l]=s[i];
-//     s[i]=temp;
-//     return s;
-// }
-
-// void permutation(string s,int l=0,int r=-1){
-//     if(r==-1)r=s.length()-1;
-//     if(l==r){
-//         cout<<s<<endl;
-//         return;
-//     }
-//     for(int i=l;i<=r;i++){
-//         s = swap(s,l,i);
-//         permutation(s,l+1,r);
-//         s = swap(s,l,i);
-//     }
-// }
-
-// int main(){
-//     // powerset("abc");
-//     permutation("abc");
-//     return 0;
-// }
-
-// #include<bits/stdc++.h>
-// using namespace std;
-
-
-// int main(){
-//     long long int n;
-//     cin>>n;
-//     long long int ans=0;
-//     long long int x=1;
-//     while(n){
-//         if(n&1){
-//             ans += x;
-//         }
-//         x *= 10;
-//         n = n>>1;
-//         cout<<ans<<endl;
-//     }
-//     cout<<ans<<endl;
-
-//     return 0;
-// }
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int isPrime(int n){
+	if(n==1) return 2;
+	if(n==2) return 1;
+	for(int i=2;i<=sqrt(n);i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+int prime(int x){
+	int ans;
+	while(!isPrime(x)){
+		x++;
+	}
+	ans = x;
+	return x;
+}
+int main() {
+	int num;
+	cin >> num;    //Reading input from STDIN
+	while(num){
+		int a;
+		cin>>a;
+		int k;
+		int s;
+		cin>>k;
+		cin>>s;
+		int l= min(k,s);
+		int r = max (k,s);
+		int p=0;
+		if(isPrime(a)) p =a;
+		if(p!=a){
 
+		for(int i=l;i<=r;i++){
+			if(isPrime(i)){
+				p = i;
+				break;
+			}
+		}
+		if(abs(p-k)<abs(p-s)) cout<<"KANISHK"<<endl;
+		else if(abs(p-k)>abs(p-s)) cout<<"SURENDER"<<endl;
+		else cout<<"DRAW"<<endl;
+		}
 
-int main(){
-    // cout<<sqrt(9)<<endl;
-    int x = sqrt(9);
-    cout<<x<<endl;
-
-    return 0;
+		num--;
+	}
+	return 0;
 }
